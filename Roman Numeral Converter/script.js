f5b13b3e6eca388882267b7da394f2135ceda68c
TRANZCEND3NCE <154397218+TRANZCEND3NCE@users.noreply.github.com>
2025-02-26 21:20:10 -0500

    Update script.js

--- Roman Numeral Converter/script.js
+++ Roman Numeral Converter/script.js
@@ -1,83 +1,42 @@
-const input = document.getElementById("number")
-const convertBtn = document.getElementById("convert-btn")
-const output = document.getElementById("output")
-let numeralRoman = ""
+const input = document.getElementById('number');
+const convertBtn = document.getElementById('convert-btn');
+const output = document.getElementById('output');
+let romanStr = '';
 const romanNumeral = [
-  {
-    input: 1000,
-    numeral: "M",
-  },
-  {
-    input: 900,
-    numeral: "CM",
-  },
-  {
-    input: 500,
-    numeral: "D",
-  },
-  {
-    input: 400,
-    numeral: "CD",
-  },
-  {
-    input: 100,
-    numeral: "C",
-  },
-  {
-    input: 90,
-    numeral: "XC",
-  },
-  {
-    input: 50,
-    numeral: "L"
-  },
-  {
-    input: 40,
-    numeral: "XL",
-  },
-  {
-    input: 10,
-    numeral: "X",
-  },
-  {
-    input: 9,
-    numeral: "IX",
-  },
-  {
-    input: 5,
-    numeral: "V",
-  },
-  {
-    input: 4,
-    numeral: "IV",
-  },
-  {
-    input: 1,
-    numeral: "I",
-  },
+  { input: 1000, numeral: 'M' },
+  { input: 900, numeral: 'CM' },
+  { input: 500, numeral: 'D' },
+  { input: 400, numeral: 'CD' },
+  { input: 100, numeral: 'C' },
+  { input: 90, numeral: 'XC' },
+  { input: 50, numeral: 'L' },
+  { input: 40, numeral: 'XL' },
+  { input: 10, numeral: 'X' },
+  { input: 9, numeral: 'IX' },
+  { input: 5, numeral: 'V' },
+  { input: 4, numeral: 'IV' },
+  { input: 1, numeral: 'I' },
 ];
 
-
 convertBtn.addEventListener('click', (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-
-  if (input.value === "") {
-    output.innerText = "Please enter a valid number"
-    return
-  } else if (input.value < 0) {
-    output.innerText = "Please enter a number greater than or equal to 1"
-    return
-  } else if (input.value > 3999) {
-    output.innerText = "Please enter a number less than or equal to 3999"
-   return
-  } 
-     toRoman(input.value);
-  output.innerText = numeralRoman;
+  if (input.value === '') {
+    output.innerText = 'Please enter a valid number';
+    return;
+  } if (input.value < 0) {
+    output.innerText = 'Please enter a number greater than or equal to 1';
+    return;
+  } if (input.value > 3999) {
+    output.innerText = 'Please enter a number less than or equal to 3999';
+    return;
+  }
+  decimalToRoman(input.value);
+  output.innerText = romanStr;
 });
 
-const toRoman = (input) => {
-  let result = "";
+const decimalToRoman = (input) => {
+  let result = '';
   let remaining = input;
 
   for (const { input, numeral } of romanNumeral) {
@@ -86,5 +45,5 @@ const toRoman = (input) => {
       remaining -= input;
     }
   }
-numeralRoman = result;
+  romanStr = result;
 };
